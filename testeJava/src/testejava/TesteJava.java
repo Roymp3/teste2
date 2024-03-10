6bc02237401b859f0a45cddd04bcac032a5138b4
Desktop <Desktop@DESKTOP-URSS2BM>
2024-03-10 15:48:22 -0300

    FILE ADD

--- testeJava/src/testejava/TesteJava.java
+++ testeJava/src/testejava/TesteJava.java
@@ -7,6 +7,8 @@ public class TesteJava {
         
         System.out.println("Hello world");
         System.out.println("Hello MUNDOI");
+        System.out.println("Hello MUNDOI");
+        System.out.println("Hello MUNDOI");
         
     }
     
