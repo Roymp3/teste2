b28819b1e507ad6cad5594f34583f5ea8629ded3
Desktop <Desktop@DESKTOP-URSS2BM>
2024-03-10 15:55:26 -0300

    FILE ADd ola

--- testeJava/src/testejava/TesteJava.java
+++ testeJava/src/testejava/TesteJava.java
@@ -5,10 +5,7 @@ public class TesteJava {
     public static void main(String[] args) {
         // TTESTEE
         
-        System.out.println("Hello world");
-        System.out.println("Hello MUNDOI");
-        System.out.println("Hello MUNDOI");
-        System.out.println("Hello MUNDOI");
+        System.out.println("Ola Roy");
         
     }
     
